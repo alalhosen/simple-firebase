8c01d47372a658f14b2f8f6ffad8eae38fcb3be2
Alal Hosen <alalhosen1992620120@gmail.com>
2025-02-04 12:40:46 +0600

    some text add

--- src/__steps__.js
+++ src/__steps__.js
@@ -3,6 +3,7 @@
  * 2. console.firebase.google.com create a project
  * 3. Register project
  * 4. get firebase config and do not save config on the client side
- * 5. exoport app form the firebase init file
  * 6. create auth where it will be added
+ * 5. exoport auth form the init file
+ * 6. in the console authentication > started > enable goole provider
  */
