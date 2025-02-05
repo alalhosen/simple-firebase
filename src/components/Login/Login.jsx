b2153af8f24aa08238520ce23220c861eb51589e
Alal Hosen <alalhosen1992620120@gmail.com>
2025-02-05 23:34:44 +0600

    set useState

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,19 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../../firebase/firebase.init";
-
+import { useState } from "react";
 
 const Login = () => {
-
-const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState(null);
+  const provider = new GoogleAuthProvider();
 
   const handleGoogleSignIn = () => {
-  signInWithPopup(auth, provider)
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log("ERROR", error);
+      });
   };
   return (
     <div>
