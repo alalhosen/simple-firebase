5eb10dc49e3200ca50646f168090420ba162448d
Alal Hosen <alalhosen1992620120@gmail.com>
2025-02-06 20:29:46 +0600

    set useState in simple firebase2

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,16 +3,19 @@ import { auth } from "../../firebase/firebase.init";
 import { useState } from "react";
 
 const Login = () => {
-  const [user, setUser] = useState(null);
+
   const provider = new GoogleAuthProvider();
+const [user, setUser]=useState(null);
 
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
-        console.log(result);
+        console.log(result.user);
+        setUser(result.user);
       })
       .catch((error) => {
         console.log("ERROR", error);
+        setUser(null);
       });
   };
   return (
